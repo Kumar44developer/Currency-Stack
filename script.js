178515b4702dc1bbf0f079b14ccc4b6220ae99ac
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-21 14:24:05 +0530

    applied parallax transform to layers

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ function effect(e){
     let x = (window.innerWidth - e.pageX * movement) /250;
     let y = (window.innerWidth - e.pageX * movement) /250;
 
+    layer.style.transform = `translate(${-x}px) translateY(${y}px)`;
+
+
 
 
